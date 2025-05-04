ecdd8004ed566568f120cfe1c2e179ac86f71293
Lam Nguyen <lamnguyendnvncn@gmail.com>
2025-05-04 01:17:27 -0400

    remove unncessary files

--- utils/lmStudio.ts
+++
@@ -1,7 +0,0 @@
-import { LMStudioClient } from "@lmstudio/sdk";
-const client = new LMStudioClient();
-
-const model = await client.llm.model("deepseek-r1-distill-qwen-7b");
-const result = await model.respond("What is the capital of France?");
-
-console.log(result);
